672ed365fad24ea862ef1fac616351d15a14ed44
Avinash Chamarthy <sc2459@njit.edu>
2021-08-02 00:49:30 -0500

    Task: added /contact route

--- app/app.py
+++ app/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, make_response, request, jsonify, render_template
+from flask import Flask, make_response, request, jsonify, render_template, redirect, url_for
+from forms import ContactForm
+import os
 
-app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+
+app = Flask(__name__, instance_relative_config=False)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 @app.route("/", methods=['GET'])
@@ -12,19 +17,34 @@ def hello():
     return make_response(jsonify(my_dict), 200, headers)
 
 
+nav = [
+    {'name': 'Home', 'url': 'https://example.com/1'},
+    {'name': 'About', 'url': 'https://example.com/2'},
+    {'name': 'Pics', 'url': 'https://example.com/3'}
+]
+
+
 @app.route('/home')
 def home():
-    nav = [
-        {'name': 'Home', 'url': 'https://example.com/1'},
-        {'name': 'About', 'url': 'https://example.com/2'},
-        {'name': 'Pics', 'url': 'https://example.com/3'}
-    ]
     """Landing page."""
     return render_template(
         'home.html',
         nav=nav,
         title="Jinja Demo Site",
         description="Smarter page templates with Flask & Jinja."
+    )
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    """Standard `contact` form."""
+    form = ContactForm()
+    if form.validate_on_submit():
+        return redirect(url_for("success"))
+    return render_template(
+        "contact.jinja2", nav=nav,
+        form=form,
+        template="form-template"
     )
 
 
