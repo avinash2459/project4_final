20f42124a23fa8053404976754f70ba6470dd9d8
Avinash Chamarthy <sc2459@njit.edu>
2021-08-01 23:33:20 -0500

    Task: Created /home route and added home.html and home.css

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, request, jsonify
+from flask import Flask, make_response, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -10,6 +10,22 @@ def hello():
     my_dict = {'key': 'dictionary value'}
     headers = {"Content-Type": "application/json"}
     return make_response(jsonify(my_dict), 200, headers)
+
+
+@app.route('/home')
+def home():
+    nav = [
+        {'name': 'Home', 'url': 'https://example.com/1'},
+        {'name': 'About', 'url': 'https://example.com/2'},
+        {'name': 'Pics', 'url': 'https://example.com/3'}
+    ]
+    """Landing page."""
+    return render_template(
+        'home.html',
+        nav=nav,
+        title="Jinja Demo Site",
+        description="Smarter page templates with Flask & Jinja."
+    )
 
 
 if __name__ == '__main__':
