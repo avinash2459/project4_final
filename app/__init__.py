8ee0cac4ab377c9009da4cb59f2da543e1165c1c
Avinash Chamarthy <sc2459@njit.edu>
2021-08-02 05:27:19 -0500

    Task: Created a blueprint method

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,17 @@
+"""Initialize Flask app."""
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_redis import FlaskRedis
 
 
-# Globally accessible libraries
-db = SQLAlchemy()
-r = FlaskRedis()
-
-
-def init_app():
-    """Initialize the core application."""
+def create_app():
+    """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
-    # Initialize Plugins
-    db.init_app(app)
-    r.init_app(app)
-
     with app.app_context():
-        # Include our Routes
-        from . import routes
+        # Import parts of our application
+        from .home import routes
 
         # Register Blueprints
+        app.register_blueprint(home.home_bp)
 
         return app
